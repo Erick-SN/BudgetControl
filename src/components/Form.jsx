ed577b4b4d32d344ae45498e8f098831d1a2f767
Erick-SN <ericksn0707@gmail.com>
2020-06-25 12:45:37 -0500

    Form state creation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,25 +1,41 @@
 import React, { useState } from 'react';
 const Form = () => {
+  const [name, setName] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  const onSubmitExpense = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <>
-      <h2>Add your expenses here</h2>
-      <div className='campo'>
-        <label>Expense Name</label>
+      <form onSubmit={onSubmitExpense}>
+        <h2>Add your expenses here</h2>
+        <div className='campo'>
+          <label>Expense Name</label>
+          <input
+            type='text'
+            className='u-full-width'
+            placeholder='Example : Transport'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className='campo'>
+          <label>Expense Amount</label>
+          <input
+            type='number'
+            className='u-full-width'
+            value={amount}
+            onChange={(e) => setAmount(parseInt(e.target.value, 10))}
+          />
+        </div>
         <input
-          type='text'
-          className='u-full-width'
-          placeholder='Example : Transport'
+          type='submit'
+          className='button-primary u-full-width'
+          value='Add'
         />
-      </div>
-      <div className='campo'>
-        <label>Expense Amount</label>
-        <input type='number' className='u-full-width' />
-      </div>
-      <input
-        type='submit'
-        className='button-primary u-full-width'
-        value='Add'
-      />
+      </form>
     </>
   );
 };
