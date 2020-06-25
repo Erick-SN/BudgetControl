789700516714792251bcf69746f6150aa20e3088
Erick-SN <ericksn0707@gmail.com>
2020-06-25 12:50:29 -0500

    Set error message at Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
+import Error from './Error';
 const Form = () => {
   const [name, setName] = useState('');
   const [amount, setAmount] = useState(0);
+  const [error, setError] = useState(false);
 
   const onSubmitExpense = (e) => {
     e.preventDefault();
+    if (amount < 1 || isNaN(amount) || name.trim() === '') {
+      setError(true);
+      return;
+    }
   };
 
   return (
     <>
       <form onSubmit={onSubmitExpense}>
         <h2>Add your expenses here</h2>
+        {error ? <Error message='Oops.. Something went wrong' /> : null}
         <div className='campo'>
           <label>Expense Name</label>
           <input
